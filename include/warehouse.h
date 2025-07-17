4014d0fba16c4a75ecec1e6fe4ece8d88669c706
kEzorka <fateevkonstantin5@gmail.com>
2025-07-17 21:53:08 +0300

    add opportunity to get cell in warehouse

--- include/warehouse.h
+++ include/warehouse.h
@@ -1,19 +1,31 @@
 #pragma once
-#include <string>
-
 #include "warehouse-cell.h"
 #include "field.h"
 
 class Warehouse {
 public:
-    explicit Warehouse() = default;
+    Warehouse() = default;
     explicit Warehouse(const std::string& name, std::pair<int, int> fieldSize = {0,0});
+    Warehouse(const Warehouse&) = default;
+    Warehouse& operator=(const Warehouse&) = default;
+    Warehouse(Warehouse&&) = default;
+    Warehouse& operator=(Warehouse&&) = default;
+    ~Warehouse() = default;
 
     void setName(const std::string& name);
     void setSize(std::pair<int, int> size);
 
     std::string getName() const;
     std::pair<int, int> getSize() const;
+
+    WarehouseCell* operator[](std::pair<size_t, size_t> pos);
+    const WarehouseCell* operator[](std::pair<size_t, size_t> pos) const;
+    std::vector<WarehouseCell*> operator[](size_t row);
+    const std::vector<WarehouseCell*> operator[](size_t row) const;
+    WarehouseCell* at(std::pair<size_t, size_t> pos);
+    const WarehouseCell* at(std::pair<size_t, size_t> pos) const;
+    WarehouseCell* at(size_t row, size_t col);
+    const WarehouseCell* at(size_t row, size_t col) const;
 
 private:
     Warehouse(const Field<WarehouseCell*>& field, const std::string& name = "");
--- source/warehouse.cpp
+++ source/warehouse.cpp
@@ -1,4 +1,5 @@
 #include "../include/warehouse.h"
+#include <utility>
 
 Warehouse::Warehouse(const std::string& name, std::pair<int, int> size) :
     name_(name), field_(Field<WarehouseCell*>(size)) {}
@@ -6,18 +7,47 @@ Warehouse::Warehouse(const std::string& name, std::pair<int, int> size) :
 Warehouse::Warehouse(const Field<WarehouseCell*>& field, const std::string& name) : 
     name_(name), field_(field) {}
 
+
 void Warehouse::setName(const std::string& name) {
     name_ = name;
 }
-
 void Warehouse::setSize(std::pair<int, int> size) {
     field_.resize(size);
 }
 
+
 std::string Warehouse::getName() const {
     return name_;
 }
-
 std::pair<int, int> Warehouse::getSize() const {
     return field_.size();
+}
+
+
+WarehouseCell* Warehouse::operator[](std::pair<size_t, size_t> pos) {
+    return field_[pos];
+}
+const WarehouseCell* Warehouse::operator[](std::pair<size_t, size_t> pos) const {
+    return field_[pos];
+}
+
+std::vector<WarehouseCell*> Warehouse::operator[](size_t row) {
+    return field_[row];
+}
+const std::vector<WarehouseCell*> Warehouse::operator[](size_t row) const {
+    return field_[row];
+}
+
+WarehouseCell* Warehouse::at(std::pair<size_t, size_t> pos) {
+    return field_[pos];
+}
+const WarehouseCell* Warehouse::at(std::pair<size_t, size_t> pos) const {
+    return field_[pos];
+}
+
+WarehouseCell* Warehouse::at(size_t row, size_t col) {
+    return field_.at(row, col);
+}
+const WarehouseCell* Warehouse::at(size_t row, size_t col) const {
+    return field_.at(row, col);
 }
